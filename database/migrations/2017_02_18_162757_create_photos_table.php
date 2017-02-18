b0ed3daf908630533bdfec4ce4cabd6aac486003
david <dpark543107@gmail.com>
2017-02-18 10:57:32 -0600

    added photo migration, added relationships between photo and user

--- database/migrations/2017_02_18_162757_create_photos_table.php
+++ database/migrations/2017_02_18_162757_create_photos_table.php
@@ -15,6 +15,8 @@ class CreatePhotosTable extends Migration
     {
         Schema::create('photos', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->string('photoAddress');
             $table->timestamps();
         });
     }
